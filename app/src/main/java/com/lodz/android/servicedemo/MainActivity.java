c09d982f49ff8c2c5be222417a806dc33266bead
LZ9 <lz.mms@foxmail.com>
2017-12-01 10:43:34 +0800

    Initial commit

---
+++ app/src/main/java/com/lodz/android/servicedemo/MainActivity.java
@@ -0,0 +1,18 @@
+package com.lodz.android.servicedemo;
+
+import android.os.Bundle;
+
+import com.lodz.android.component.base.activity.AbsActivity;
+
+public class MainActivity extends AbsActivity {
+
+    @Override
+    protected int getAbsLayoutId() {
+        return R.layout.activity_main;
+    }
+
+    @Override
+    protected void findViews(Bundle savedInstanceState) {
+
+    }
+}
